9453f2fa90dde443172dce2d3933b860286145b8
Ben Keith <ben@benlk.com>
2023-10-28 16:36:40 -0400

    October 28 run

--- scraper.bash
+++ scraper.bash
@@ -2,7 +2,7 @@
 
 # https://stackoverflow.com/a/24967128
 
-for num in {1..999}; do
+for num in {400..599}; do
 	# build URL and try to download
 	URL="https://public.govdelivery.com/topics/OHCCC_$num/feed.rss"
 	FILE="./rss/OHCCC_$num.rss"
