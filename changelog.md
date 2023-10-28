81e56ed61a6cfeee6d7742bb5d8ea0e9b822b32a
Ben Keith <ben@benlk.com>
2023-10-28 16:43:43 -0400

    Remove unused columns in feeds.csv

--- changelog.md
+++ changelog.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html), as much as a document can.
 
+## 3.0
+
+### Deprecated
+
+- Removed the "Most-recent update" and "Count" columns in `feeds.csv` in order to simplify diffs and reflect the fact the feeds update more recently than this repo.
+
 ## 2.01
 
 2022-12-31
--- parser.bash
+++ parser.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '"Feed Number","Title","Link","Description","Feed URL","Newsletter Signup URL","Most-Recent Date","Number of items"'
+echo '"Feed Number","Title","Link","Description","Feed URL","Newsletter Signup URL"'
 
 for file in ./rss/*.rss
 do
@@ -8,14 +8,12 @@ do
 	TITLE=$(cat $file | xq .rss.channel.title )
 	LINK=$(cat $file | xq .rss.channel.link )
 	DESCRIPTION=$(cat $file | xq .rss.channel.description )
-	RECENT=$(cat $file | xq '.rss.channel.item[0].pubDate' )
-	COUNT=$(cat $file | xq '.rss.channel.item | keys | length' )
 
 	NUMBER=$( echo $file | sed -r 's%./rss/OHCCC_([[:alnum:]]+)\.rss%\1%')
 
 	FEEDURL="https://public.govdelivery.com/topics/OHCCC_$NUMBER/feed.rss"
 	SIGNUP="https://public.govdelivery.com/accounts/OHCCC/subscriber/new?qsp=OHCCC_$NUMBER"
 
-	CSV_LINE="\"$NUMBER\",$TITLE,$LINK,$DESCRIPTION,\"$FEEDURL\",$SIGNUP,$RECENT,$COUNT"
+	CSV_LINE="\"$NUMBER\",$TITLE,$LINK,$DESCRIPTION,\"$FEEDURL\",$SIGNUP"
 	echo $CSV_LINE
 done
