4ffecf405706f1478ac75300df18bf7d115bbc76
Ben Keith <ben@benlk.com>
2026-01-05 22:39:50 -0500

    06 Jan 2026 run; shorten needed scraper runtime

--- scraper.bash
+++ scraper.bash
@@ -4,7 +4,7 @@
 
 set -u
 
-for num in {500..699}; do
+for num in {610..699}; do
 	# build URL and try to download
 	URL="https://public.govdelivery.com/topics/OHCCC_$num/feed.rss"
 	FILE="./rss/OHCCC_$num.rss"
