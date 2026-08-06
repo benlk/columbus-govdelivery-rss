5a2972dc2df49de4d30fcdb2371a91d30eb56b13
Ben Keith <ben@benlk.com>
2026-08-06 12:56:45 -0400

    Abort update early if no new RSS files

--- update.bash
+++ update.bash
@@ -2,6 +2,12 @@
 set -euo pipefail
 bash ./scraper.bash
 
+# Use git to check if there are any changes in the ./rss directory
+if git diff --quiet --exit-code ./rss; then
+    echo "No new feeds found. Exiting."
+    exit 0;
+fi
+
 # Append new lines to feeds.csv
 bash ./parser.bash >> feeds.csv
 
