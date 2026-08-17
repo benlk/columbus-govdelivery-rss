c1c38b9dbd69f42d6b97a88082c2ab2b0a1a1176
Ben <ben@benlk.com>
2026-08-17 17:53:55 -0400

    Correct test used to determine whether to regenerate the OPML file

--- update.bash
+++ update.bash
@@ -3,7 +3,7 @@ set -euo pipefail
 bash ./scraper.bash
 
 # Use git to check if there are any changes in the ./rss directory
-if git diff --quiet --exit-code ./rss; then
+if test -z "$(git ls-files --others ./rss)"; then
     echo "No new feeds found. Exiting."
     exit 0;
 fi
