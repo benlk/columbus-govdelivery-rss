71669161cc9c8286a8cd704466b3690a44918e86
Ben <ben@benlk.com>
2026-08-22 00:12:54 -0400

    Fix generation of CSV from RSS files

--- update.bash
+++ update.bash
@@ -1,18 +1,19 @@
 #! /usr/bin/env bash
 set -euo pipefail
-bash ./scraper.bash
+#bash ./scraper.bash
 
 # Use git to check if there are any changes in the ./rss directory
-if test -z "$(git ls-files --others ./rss)"; then
-    echo "No new feeds found. Exiting."
-    exit 0;
-fi
+#if test -z "$(git ls-files --others ./rss)"; then
+#    echo "No new feeds found. Exiting."
+#    exit 0;
+#fi
 
 # Append new lines to feeds.csv
 bash ./parser.bash >> feeds.csv
 
 # Remove duplicate lines from feeds.csv
-uniq -u feeds.csv > feeds.tmp && mv feeds.tmp feeds.csv
+HEADER='"Feed Number","Title","Link","Description","Feed URL","Newsletter Signup URL"'
+(printf '%s\n' "$HEADER" && awk -v header="$HEADER" '$0 != header' feeds.csv | sort -u) > feeds.tmp && mv feeds.tmp feeds.csv
 
 php subscriptions.opml.php > subscriptions.opml
 git status
